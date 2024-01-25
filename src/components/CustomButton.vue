1a341fc614c9e3dadb724079284a1bcfc4cb5f98
Alejandro Yunes <alejandroyunes@outlook.com>
2024-01-24 20:08:17 -0500

    fixed arquitecture

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="custom-button">{{ text }}</button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CustomButton',
+  props: {
+    text: String,
+    color: String
+  },
+  methods: {
+    onClick() {
+      console.log('button being clicked')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.custom-button {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <CustomButton text="click me" color="red" />
+  </header>
+</template>
+
+<script lang="ts">
+import CustomButton from './CustomButton.vue';
+
+export default {
+  name: 'PageHeader',
+  props: {
+    title: {
+      type: String,
+      default: 'This is the default title',
+    },
+  },
+  components: {
+    CustomButton,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+}
+</style>
